3eec497e60818d9eb14d0acfa40ad53eb21bf292
shau8122 <shau8122@kgpian.iitkgp.ac.in>
2024-02-26 20:37:56 +0530

    url fixes

--- app.js
+++ app.js
@@ -18,25 +18,10 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.set("trust proxy", 1);
 
-// const allowedOrigins = [
-//     'http://localhost:3000', // Replace this with your first frontend URL
-//     'https://localhost:3001',   // Replace this with your second frontend URL
-//   ];
 
-// // cors cofiguration
-// if (process.env.NODE_ENV !== "PRODUCTION") {
-//     app.use(require('cors')({
-//         origin: true,
-//         allowedHeaders: ['Content-Type', 'Authorization'],
-//         credentials:true,
-//         methods: '*' ,
-//         optionsSuccessStatus: 200,
-//     }))
-// }
 const cors = require("cors");
-
 const corsOptions = {
-  origin: true, // Allow requests from any origin
+  origin: ["http://localhost:5173", "https://jkbros.netlify.app"], // Allow requests from any origin
   credentials: true, // Allow cookies across origins
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Specify allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"], // Specify allowed headers
@@ -48,11 +33,6 @@ app.use(cors(corsOptions));
 app.use("/api/v1", userRoute);
 app.use("/api/v1", orderRoute);
 
-// app.use(express.static(path.join(__dirname + "./../frontend/build")));
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "./../frontend/build/index.html"));
-// })
 
 // error middileware
 app.use(errorMiddleware);
